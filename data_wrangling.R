02608fac9c04d48eaccce1b87db8edeb9ff1cfde
aliciacanales <canalesalicia06@gmail.com>
2024-02-27 21:56:02 -0800

    datasets that I made

---
+++ data_wrangling.R
@@ -0,0 +1,13 @@
+ncos_nesting <- data.frame(year = c(2018, 2019, 2020, 2021, 2022, 2023),
+                               number_of_nests = c(1, 3, 1, 0, 3, 0)) ## UCSB North Campus Open Space (NCOS)
+ellwood_nesting <- data.frame(year = c(2019, 2020, 2021, 2022, 2023),
+                              number_of_nests = c(1, 0, 0, 0, 2)) ## ellwood 
+ormond_beach_nesting <- data.frame(year = c(seq(2003, 2023, 1)),
+                                   number_of_nests = c(15, 12, 14, 22, 13, 38, 18, 19, 18, 20, 1, 
+                                                       5, 19, 12, 23, 23, 42, 31, 23, 31, 53)) ## nesting data that hatched 
+hollywood_beach_nesting <- data.frame(year = c(seq(1994, 2016, 1)),
+                                      number_of_nests = c(218, 211, 234, 239, 132, 78, 105, 122, 
+                                                          201, 282, 420, 275, 269, 171, 207, 173, 
+                                                          174, 216, 205, 220, 198, 269, 223))
+adults_during_breeding <- data.frame(year = c(1980:seq(1989, 2006,1)) ,
+                                     number_of_nests = c(80, 126, 87, 85, 113, 150, 172, 268, 259 , 167))              
